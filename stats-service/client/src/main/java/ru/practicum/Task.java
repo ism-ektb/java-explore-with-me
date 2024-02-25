7f31b47c8852160c3bf06127f7040c7c705d5a50
ism <ism-ektb@yandex.ru>
2024-02-25 18:01:11 +0500

    6

--- stats-service/client/src/main/java/ru/practicum/Task.java
+++ stats-service/client/src/main/java/ru/practicum/Task.java
@@ -1,7 +1,6 @@
 package ru.practicum;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
